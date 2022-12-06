a968aac50cb1d50c1c0fd47df06da23e26a3ec5d
Dawid <d.j.kopiczko@student.vu.nl>
2022-12-06 17:27:06 +0100

    add d-separation

--- BNReasoner.py
+++ BNReasoner.py
@@ -2,6 +2,7 @@ from typing import Union
 from BayesNet import BayesNet
 from typing import List, Dict
 import pandas as pd
+import networkx as nx
 
 
 class BNReasoner:
@@ -17,20 +18,11 @@ class BNReasoner:
         else:
             self.bn = net
 
-    # TODO: This is where your methods should go
-    def prune_leafs_rec(self, target_vars: List[str]):
-        leafs = [node for node in self.bn.structure.nodes() if self.bn.structure.out_degree(node) == 0]
-        leafs_to_del = [leaf for leaf in leafs if leaf not in target_vars]
-        for leaf in leafs_to_del:
-            self.bn.del_var(leaf)
-        if len(leafs_to_del) != 0:
-            self.prune_leafs_rec(target_vars)
-
     def prune_network(self, vars: List[str], e: Dict[str, bool]):
         if not all(var in self.bn.get_all_variables() for var in vars):
-            raise Exception('provided variables not in network')
+            raise Exception("provided variables not in network")
         if not all(key in self.bn.get_all_variables() for key in e.keys()):
-            raise Exception('provided evidence not part of netowrk')
+            raise Exception("provided evidence not part of netowrk")
 
         for var in e.keys():
             for child in self.bn.get_children(var):
@@ -41,8 +33,53 @@ class BNReasoner:
                     del e_without_factor[child]
                 cpt = self.bn.reduce_factor_rem_row(pd.Series(e_without_factor), cpt)
                 print(cpt)
-                cpt = cpt.drop(columns = [var])
+                cpt = cpt.drop(columns=[var])
                 # cpt = cpt[cpt['p'] != 0]
                 self.bn.update_cpt(child, cpt)
-                
-        self.prune_leafs_rec(list(set(vars).union(set(e.keys()))))
+
+        self._prune_leafs_rec(list(set(vars).union(set(e.keys()))))
+
+    def _prune_leafs_rec(self, target_vars: List[str]):
+        leafs = [
+            node
+            for node in self.bn.structure.nodes()
+            if self.bn.structure.out_degree(node) == 0
+        ]
+        leafs_to_del = [leaf for leaf in leafs if leaf not in target_vars]
+        for leaf in leafs_to_del:
+            self.bn.del_var(leaf)
+        if len(leafs_to_del) != 0:
+            self._prune_leafs_rec(target_vars)
+
+    def d_separated(self, X, Y, Z):
+        """
+        Checks if X and Y are d-separated given Z.
+        """
+        graph = self.bn.structure.copy()
+
+        while True:
+            leaves = [
+                node
+                for node in graph.nodes()
+                if graph.out_degree(node) == 0
+                and node not in Z
+                and node not in X
+                and node not in Y
+            ]
+            if len(leaves) == 0:
+                break
+            for leaf in leaves:
+                graph.remove_node(leaf)
+
+        for node in Z:
+            for child in self.bn.get_children(node):
+                graph.remove_edge(node, child)
+
+        subgraphs = nx.weakly_connected_components(graph)
+        for subgraph in subgraphs:
+            if (
+                len(set(X).intersection(subgraph)) > 0
+                and len(set(Y).intersection(subgraph)) > 0
+            ):
+                return False
+        return True
