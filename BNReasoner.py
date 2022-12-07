d5cf484b1ef34c3eebc00302c836539732e7b379
MateiAnton <anton.matei10@gmail.com>
2022-12-07 14:50:39 +0100

    Implement orderings

--- BNReasoner.py
+++ BNReasoner.py
@@ -3,6 +3,7 @@ from BayesNet import BayesNet
 from typing import List, Dict
 import pandas as pd
 import networkx as nx
+import matplotlib.pyplot as plt
 
 
 class BNReasoner:
@@ -119,3 +120,53 @@ class BNReasoner:
         merged["p"] = ps
 
         return merged
+
+    def min_degree_ordering(self):
+        interaction_graph = self.bn.get_interaction_graph()
+        ordering = []
+        while len(interaction_graph.nodes()) > 0:
+            nx.draw(interaction_graph, with_labels=True, node_size=3000)
+            plt.show()
+            min_degree_node = min(
+                interaction_graph.nodes(), key=interaction_graph.degree
+            )
+            ordering.append(min_degree_node)
+            # get neighbours of min_degree_node
+            neighbours = list(interaction_graph.neighbors(min_degree_node))
+            # connect the neighbours of min_degree_node
+            for i in range(len(neighbours)):
+                for j in range(i + 1, len(neighbours)):
+                    interaction_graph.add_edge(neighbours[i], neighbours[j])
+            interaction_graph.remove_node(min_degree_node)
+        return ordering
+
+    def min_fill_ordering(self):
+        interaction_graph = self.bn.get_interaction_graph()
+        ordering = []
+        while len(interaction_graph.nodes()) > 0:
+            # nx.draw(interaction_graph, with_labels=True, node_size=3000)
+            # plt.show()
+            min_fill_node = min(
+                interaction_graph.nodes(),
+                key=lambda node: self._nr_of_new_interactions_after_deletion(
+                    node, interaction_graph
+                ),
+            )
+            ordering.append(min_fill_node)
+            # get neighbours of min_degree_node
+            neighbours = list(interaction_graph.neighbors(min_fill_node))
+            # connect the neighbours of min_degree_node
+            for i in range(len(neighbours)):
+                for j in range(i + 1, len(neighbours)):
+                    interaction_graph.add_edge(neighbours[i], neighbours[j])
+            interaction_graph.remove_node(min_fill_node)
+        return ordering
+
+    def _nr_of_new_interactions_after_deletion(self, node, interaction_graph):
+        neighbours = list(interaction_graph.neighbors(node))
+        nr_of_interactions = 0
+        for i in range(len(neighbours)):
+            for j in range(i + 1, len(neighbours)):
+                if not interaction_graph.has_edge(neighbours[i], neighbours[j]):
+                    nr_of_interactions += 1
+        return nr_of_interactions
